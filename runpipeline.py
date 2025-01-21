821a1d779a9f0fdfb0a37e01d22e1db3f27da8f1
DaramNikhil <nikhildaram51@gmail.com>
2025-01-21 15:49:55 +0530

    update runpipeline.py

--- runpipeline.py
+++ runpipeline.py
@@ -1,2 +1,16 @@
 import pandas as pd
 import numpy as np
+from main import read_data
+
+def process_post(data_path):
+    df = read_data(data_path)
+    return df
+
+
+
+
+
+if __name__ == "__main__":
+    data_path = "<add your data path here>"
+    df = process_post(data_path)
+    
