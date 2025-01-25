9c0ab56537993aa9ba9e88a22c5df06bd3a3bddc
DaramNikhil <nikhildaram51@gmail.com>
2025-01-25 10:56:20 +0530

    update api.py

--- api.py
+++ api.py
@@ -16,18 +16,20 @@ data = []
 @app.get('/get_data')
 def get_data():
     if data:
-        return {"data": data}
+        cleaned_data = [[record["uname"], record["password"]] for record in data]
+        return {"data": cleaned_data}
+
     else:
         return {"message": "No data found"}
 
 @app.post('/add_data')
 def add_data(item: Item):
-    data = item.dict()
-    name = item.get('name')
-    email = item.get('email')
-    password = item.get('password')
-    hashed_password = sha256(password.encode()).hexdigest()
-    user_data = {"name": name, "email": email, "hashed_password": hashed_password}
-    data.append(user_data)
+    new_data = item.dict()
+    # name = item.get('name')
+    # email = item.get('email')
+    # password = item.get('password')
+    # hashed_password = sha256(password.encode()).hexdigest()
+    # user_data = {"name": name, "email": email, "hashed_password": hashed_password}
+    data.append(new_data)
     return {"message": "Data added successfully"}
     
